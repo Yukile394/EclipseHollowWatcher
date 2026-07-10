b431219c324d6dd5234363bdb16b6fbee7eec640
Yukile394 <muhamedmuhet@gmail.com>
2026-07-10 16:25:07 +0300

    Add Herobrine model and setAngles method

--- src/client/java/com/yukile394/eclipsehollowwatcher/client/render/HerobrineModel.java
+++ src/client/java/com/yukile394/eclipsehollowwatcher/client/render/HerobrineModel.java
@@ -35,5 +35,55 @@ public class HerobrineModel extends SinglePartEntityModel<HerobrineEntity> {
         return this.root;
     }
 
-    // ...rest of the file (getTexturedModelData, setAngles) stays exactly the same
+    public static TexturedModelData getTexturedModelData() {
+        ModelData modelData = new ModelData();
+        ModelPartData root = modelData.getRoot();
+
+        root.addChild("head", ModelPartBuilder.create()
+                        .uv(0, 0).cuboid(-4.0f, -8.0f, -4.0f, 8.0f, 8.0f, 8.0f),
+                ModelTransform.pivot(0.0f, 0.0f, 0.0f));
+
+        root.addChild("body", ModelPartBuilder.create()
+                        .uv(16, 16).cuboid(-4.0f, 0.0f, -2.5f, 8.0f, 20.0f, 5.0f),
+                ModelTransform.pivot(0.0f, 0.0f, 0.0f));
+
+        root.addChild("right_arm", ModelPartBuilder.create()
+                        .uv(40, 16).cuboid(-1.5f, 0.0f, -1.5f, 3.0f, 24.0f, 3.0f),
+                ModelTransform.pivot(-5.5f, 1.0f, 0.0f));
+
+        root.addChild("left_arm", ModelPartBuilder.create()
+                        .uv(40, 44).cuboid(-1.5f, 0.0f, -1.5f, 3.0f, 24.0f, 3.0f),
+                ModelTransform.pivot(5.5f, 1.0f, 0.0f));
+
+        root.addChild("right_leg", ModelPartBuilder.create()
+                        .uv(0, 16).cuboid(-2.0f, 0.0f, -2.0f, 4.0f, 20.0f, 4.0f),
+                ModelTransform.pivot(-2.0f, 20.0f, 0.0f));
+
+        root.addChild("left_leg", ModelPartBuilder.create()
+                        .uv(0, 44).cuboid(-2.0f, 0.0f, -2.0f, 4.0f, 20.0f, 4.0f),
+                ModelTransform.pivot(2.0f, 20.0f, 0.0f));
+
+        return TexturedModelData.of(modelData, 64, 64);
+    }
+
+    @Override
+    public void setAngles(HerobrineEntity entity, float limbAngle, float limbDistance, float ageInTicks, float netHeadYaw, float headPitch) {
+        this.head.yaw = netHeadYaw * (float) (Math.PI / 180.0);
+        this.head.pitch = headPitch * (float) (Math.PI / 180.0);
+
+        float swing = MathHelper.cos(limbAngle * 0.6662f) * 1.1f * limbDistance;
+        this.rightArm.pitch = swing;
+        this.leftArm.pitch = -swing;
+        this.rightLeg.pitch = -swing;
+        this.leftLeg.pitch = swing;
+
+        float armLengthScale = entity.getArmLengthScale();
+        float armStretch = 1.0f + (armLengthScale - 1.0f) * 1.4f;
+        this.rightArm.yScale = armStretch;
+        this.leftArm.yScale = armStretch;
+
+        int moodLevel = entity.getMood().getLevel();
+        this.body.pitch = 0.05f + moodLevel * 0.015f;
+        this.head.pivotY = -2.0f - moodLevel * 0.5f;
+    }
 }
