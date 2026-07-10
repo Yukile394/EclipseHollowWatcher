9a780c899c595daa2966c2e9e6d64aefd090b245
Yukile394 <muhamedmuhet@gmail.com>
2026-07-10 16:01:39 +0300

    Delete src/client/java/com/yukile394/eclipsehollowwatcher/client/render/HerobrineRenderState.java

--- src/client/java/com/yukile394/eclipsehollowwatcher/client/render/HerobrineRenderState.java
+++
@@ -1,9 +0,0 @@
-package com.yukile394.eclipsehollowwatcher.client.render;
-
-import net.minecraft.client.render.entity.state.LivingEntityRenderState;
-
-public class HerobrineRenderState extends LivingEntityRenderState {
-    public float armLengthScale = 1.0f;
-    public int moodLevel = 0;
-    public float eyeGlowIntensity = 1.0f;
-}
