f805187f8d95b2e3285e5f74b95b92ff6e168924
Yukile394 <muhamedmuhet@gmail.com>
2026-07-10 07:36:56 +0300

    Refactor Herobrine entity registration method

--- src/main/java/com/yukile394/eclipsehollowwatcher/entity/ModEntities.java
+++ src/main/java/com/yukile394/eclipsehollowwatcher/entity/ModEntities.java
@@ -4,8 +4,6 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnGroup;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
-import net.minecraft.registry.RegistryKey;
-import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 
 public final class ModEntities {
@@ -19,7 +17,7 @@ public final class ModEntities {
                     .dimensions(0.6f, 1.8f)
                     .maxTrackingRange(96)
                     .trackingTickInterval(1)
-                    .build(RegistryKey.of(RegistryKeys.ENTITY_TYPE, HEROBRINE_ID))
+                    .build(HEROBRINE_ID.toString())
     );
 
     private ModEntities() {
