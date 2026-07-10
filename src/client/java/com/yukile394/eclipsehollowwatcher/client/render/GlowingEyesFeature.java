92349cce400638eb39b02ae98c829a6263ac8746
Yukile394 <muhamedmuhet@gmail.com>
2026-07-10 15:49:04 +0300

    Refactor GlowingEyesFeature to use HerobrineEntity

--- src/client/java/com/yukile394/eclipsehollowwatcher/client/render/GlowingEyesFeature.java
+++ src/client/java/com/yukile394/eclipsehollowwatcher/client/render/GlowingEyesFeature.java
@@ -1,5 +1,6 @@
 package com.yukile394.eclipsehollowwatcher.client.render;
 
+import com.yukile394.eclipsehollowwatcher.entity.HerobrineEntity;
 import net.minecraft.client.render.OverlayTexture;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.VertexConsumerProvider;
@@ -8,22 +9,19 @@ import net.minecraft.client.render.entity.feature.FeatureRendererContext;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
 
-public class GlowingEyesFeature extends FeatureRenderer<HerobrineRenderState, HerobrineModel> {
+public class GlowingEyesFeature extends FeatureRenderer<HerobrineEntity, HerobrineModel> {
 
     private static final Identifier EYES_TEXTURE =
             Identifier.of("eclipsehollowwatcher", "textures/entity/herobrine_eyes.png");
 
-    public GlowingEyesFeature(FeatureRendererContext<HerobrineRenderState, HerobrineModel> context) {
+    public GlowingEyesFeature(FeatureRendererContext<HerobrineEntity, HerobrineModel> context) {
         super(context);
     }
 
     @Override
     public void render(MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light,
-                        HerobrineRenderState state, float limbAngle, float limbDistance) {
-        // getEyes() renders fully bright regardless of world light level and
-        // additively blends onto the base layer - the same technique vanilla
-        // uses for Enderman eyes - which is exactly the "only the eyes are
-        // visible in total darkness" look this mod wants.
+                        HerobrineEntity entity, float limbAngle, float limbDistance, float tickDelta,
+                        float animationProgress, float headYaw, float headPitch) {
         this.getContextModel().render(matrices, vertexConsumers.getBuffer(RenderLayer.getEyes(EYES_TEXTURE)),
                 light, OverlayTexture.DEFAULT_UV);
     }
